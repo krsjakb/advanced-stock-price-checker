d3781fa630e2013abd7839fb0c682a30428e4d34
krsjakb <kb9719@gmail.com>
2024-10-05 22:14:37 +0200

    feat(stock): add stock module, service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StocksModule } from './stocks/stocks.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    StocksModule,
   ],
 })
 export class AppModule {}
