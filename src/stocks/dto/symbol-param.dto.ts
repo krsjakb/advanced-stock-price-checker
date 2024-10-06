0cac307009f1346a9cec105b0cecfc5ab4804a0e
krsjakb <kb9719@gmail.com>
2024-10-06 21:49:31 +0200

    fix(stock): add the missing apiParam and fix the validation for the input

--- src/stocks/dto/symbol-param.dto.ts
+++ src/stocks/dto/symbol-param.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class SymbolParamDto {
+  @ApiProperty({ description: 'Stock symbol', example: 'AAPL' })
   @IsNotEmpty()
   @IsString()
-  @Matches(/^[A-Za-z]+$/, { message: 'Symbol must contain only letters' })
+  @Matches(/^[A-Za-z:]+$/, {
+    message: 'Symbol must contain only letters and colons',
+  })
   symbol: string;
 }
